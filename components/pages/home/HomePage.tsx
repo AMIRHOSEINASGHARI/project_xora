4e983f3b8fe5fe4f10f7ee0b7ecbfdf2a2f7b2ae
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2025-03-22 04:08:49 +0330

    Pricing component added to HomePage

--- components/pages/home/HomePage.tsx
+++ components/pages/home/HomePage.tsx
@@ -1,11 +1,13 @@
 import Hero from "./templates/Hero";
 import Features from "./templates/Features";
+import Pricing from "./templates/Pricing";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <Features />
+      <Pricing />
     </>
   );
 };
